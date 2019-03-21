bb09404b964b8913f8f004734fda13d4ee31341c
isuruse <isuru.is77@gmail.com>
2019-03-21 09:08:38 +0530

    new post creation functionality - done

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -17,8 +17,8 @@ those methods can be automatically implement by inheriting UserMixin
 
 class User(db.Model, UserMixin): # by inherinting required methods will come to this model
     id = db.Column(db.Integer, primary_key=True)
-    username  = db.Column(db.String(20), unique=True, nullable=False) 
-    email  = db.Column(db.String(120), unique=True, nullable=False) 
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True) # setting 1-m relationship, here post is all lowercase as Post moodel(Post Class) creates a table as post(lowercase) automatically, so we need to refer that post table, not Post model.
@@ -29,8 +29,8 @@ class User(db.Model, UserMixin): # by inherinting required methods will come to 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title  = db.Column(db.String(100), nullable=False) 
-    date_posted  = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # here we dont use () as we dont want to call the function, we just need to set as after we can call when outtung it to database
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)  # here we dont use () as we dont want to call the function, we just need to set as after we can call when outtung it to database
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # here in user.id, user is all lower case as User model(User class) create tabel user(all lower) automatically, so we need to refer id in that table user, not User model. 
                                                               # if want to set specific name for table, there's a specific TABLE?NAME attribute
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_blog.models import User, Post
 from flask_blog import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
@@ -7,25 +7,26 @@ import secrets
 import os
 from PIL import Image
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Corey Schafer',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'April 20, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'April 21, 2018'
+#     }
+# ]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -108,3 +109,14 @@ def account():
     image_file = url_for('static', filename='images/' + current_user.image_file)
     return render_template('account.html', title="Account", image_file=image_file, form=form) #user name is bind directly to html uing current_user module
 
+
+@app.route("/post/new", methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template("create_post.html", form=form)
